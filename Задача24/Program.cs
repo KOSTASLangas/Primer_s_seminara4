19aef632f6c79d70f33780aa59f5576f734b343f
Kostas <langas.kostya@mail.ru>
2022-08-04 16:31:36 +0500

    Нахождение суммы чисел от 1 до n

---
+++ Задача24/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел
+// от 1 до А.
+
+int n = Convert.ToInt32(Console.ReadLine());
+int summa = 0;
+for (int i = 1; i <= n; i++)    // 1. переменная исп в цикле фор. 2. условие работы цикла. 3. счетчик ув. значение i
+    summa += i;
+Console.WriteLine(summa);
+
+// Второй способ решения задачи
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(((n + 1) / 2) * n);
