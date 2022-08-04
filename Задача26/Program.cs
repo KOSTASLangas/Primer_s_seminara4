b5019dccb63eba1d285e1a26cfcc57982bedb5e0
Kostas <langas.kostya@mail.ru>
2022-08-04 18:31:53 +0500

    Нахождение количества цифр в числе

---
+++ Задача26/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+Console.WriteLine("введи число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+while (n != 0)  
+{
+        i++;
+    n /= 10; 
+}
+Console.WriteLine(i);
